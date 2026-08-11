35af910bc6bed75fc035e382ba551eaab0146cad
OFrede <moio99@gmail.com>
2026-08-11 10:41:02 +0200

    creado o enviroment-prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+
+export enum environments {
+  dev = 1, test = 2, pre = 3, pro = 4
+};
+
 export const environment = {
-  production: true
+  production: true,
+  whereIAm: environments.pro,
+  apiUrl: "https://osteuslivrosnodejs.onrender.com/api"
 };
